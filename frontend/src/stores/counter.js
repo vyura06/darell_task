d0296f7e5adc10f41ec77f200544780e0eeebf42
vyura06 <vechorko.yura@gmail.com>
2023-12-05 15:44:35 +0300

    Client-side update: library, styles, structure.

--- frontend/src/stores/counter.js
+++ frontend/src/stores/counter.js
@@ -1,12 +1,58 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+import axios from 'axios'
+
+
+
+Vue.use(Vuex)
+
+let sections = [
+  {htmlId:'snaks', name: 'Закуски'},
+  {htmlId:'hot', name: 'Горячие блюда'},
+]
+export default new Vuex.Store({
+  state: {
+    sections,
+    sectionIds: sections.map(x=>x.htmlId)
+  },
+  getters: {
+    sections(state){
+      return state.sections
+    },
+    sectionIds(state){
+      return state.sectionIds
+    }
+  },
+  mutations: {
+    addSection(state, section){
+      if(!state.sectionIds.includes(section.htmlId)){
+        state.sectionIds.push(section.htmlId)
+        state.sections.push(section)
+      }
+    }
+  },
+  actions: {
+    async  loadSections({commit}){
+      const url = "/api/sections"
+      const response = await axios.get(url)
+      for(const section of response.data){
+        commit('addSection', section)
+      }
+    },
+    async loadDishes(){
+      const url = "/api/dishes"
+      const response = await axios.get(url)
+      const dishesBySections = []
+      for(const section of response.data){
+        dishesBySections.push({id:section.htmlId, name:section.name, dishes: section.Dish.map(x=>({section:section.htmlId,...x}))})
+      }
+      return dishesBySections
+    },
+    async sendForm(ctx, formData){
+      await axios.post('/api/dishes',formData, {  headers: { "Content-Type": "multipart/form-data" }})
+    }
+  },
+  modules: {
   }
-
-  return { count, doubleCount, increment }
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+	transpileDependencies: true,
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: bootstrapSassAbstractsImports.join('\n')
+			},
+			scss: {
+				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+			}
+		}
+	},
+	devServer:{
+		proxy: {
+			"api/":{
+				target: "http://localhost:3000",
+				secure: false
+			},
+			"img/":{
+				target: "http://localhost:3000",
+				secure: false
+			}
+		}
+	}	
 })
+	
